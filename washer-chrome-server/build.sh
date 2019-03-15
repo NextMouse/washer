ffedcb759ff7546d036b25978a0d8eea18857cc6
litaiqing <litaiqing@analysys.com.cn>
2019-03-15 14:27:44 +0800

    update

--- washer-chrome-server/build.sh
+++ washer-chrome-server/build.sh
@@ -4,15 +4,15 @@
 
 echo "build starting..."
 FILE_NAME=server.jar
-artifactId=`awk '/<artifactId>[^<]+<\/artifactId>/{gsub(/<artifactId>|<\/artifactId>/,"",$1);print $1;exit;}' ../pom.xml`
-version=`awk '/<version>[^<]+<\/version>/{gsub(/<version>|<\/version>/,"",$1);print $1;exit;}' ../pom.xml`
+artifactId=`awk '/<artifactId>[^<]+<\/artifactId>/{gsub(/<artifactId>|<\/artifactId>/,"",$1);print $1;exit;}' pom.xml`
+version=`awk '/<version>[^<]+<\/version>/{gsub(/<version>|<\/version>/,"",$1);print $1;exit;}' pom.xml`
 echo " ==> $artifactId-$version"
 [ ! -e "$FILE_NAME" ] && {
     echo " ==> mvn package..."
     mv clean package
-    cp ../target/$artifactId-$version.jar ./FILE_NAME.jar
+    cp ./target/$artifactId-$version.jar ./FILE_NAME.jar
 }
 echo " ==> build Dockerfile..."
 docker build -t networkfox/chrome-java-server:$version .
-#rm -f server.jar
+rm -f server.jar
 echo "build end!"
